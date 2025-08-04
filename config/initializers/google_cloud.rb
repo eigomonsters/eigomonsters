9f9d45471078be5edfe1d49738923f1b8e3dadeb
eigomonsters <eigomonsters@gmail.com>
2025-08-04 20:06:56 +0900

    gcp_key added

--- config/initializers/google_cloud.rb
+++ config/initializers/google_cloud.rb
@@ -1,14 +1,17 @@
+require "json"
 require "google/cloud/firestore"
 require "google/cloud/storage"
 
+credentials = JSON.parse(ENV["GOOGLE_CREDENTIALS_JSON"])
+
 FIRESTORE = Google::Cloud::Firestore.new(
-  project_id: ENV['GCP_PROJECT_ID'],
-  credentials: ENV['GCP_CREDENTIALS_PATH']
+  project_id: ENV["GCP_PROJECT_ID"],
+  credentials: credentials
 )
 
 STORAGE = Google::Cloud::Storage.new(
-  project_id: ENV['GCP_PROJECT_ID'],
-  credentials: ENV['GCP_CREDENTIALS_PATH']
+  project_id: ENV["GCP_PROJECT_ID"],
+  credentials: credentials
 )
 
-STORAGE_BUCKET = STORAGE.bucket(ENV['FIREBASE_STORAGE_BUCKET']) # 例: myapp.appspot.com
+STORAGE_BUCKET = STORAGE.bucket(ENV["FIREBASE_STORAGE_BUCKET"])
